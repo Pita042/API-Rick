37a0135371a60265c7e993bb99b00f6d83700c64
Pita042 <vitor190499@edu.unipar.br>
2024-09-10 22:29:19 -0300

    mudança

---
+++ script.js
@@ -0,0 +1,31 @@
+const apiUrl = "https://rickandmortyapi.com/api/character"
+
+function createList(Personagens) {
+    const listaHTML = Personagens.map((personagem) => {
+        console.log(personagem)
+        return `
+         <div class="personagem">
+            <img src="${personagem.image}" alt="${personagem.name}">
+            <div class="personagem_conteudo">
+                <h3>${personagem.name}</h3>
+                <span id="status">Status</span>
+            </div>
+        </div>
+        `
+    })
+    document.getElementById('lista-personagens').innerHTML = listaHTML.join();
+}
+
+
+async function getPersonagens(search){
+    const filter = search  ? "?name="+search : '';
+    const response = await fetch(apiUrl+filter)
+    const{results} = await response.json()
+    createList(results)
+}
+
+document.getElementById('search').addEventListener('keyup', (e) =>{
+    getPersonagens(e.target.value)
+})
+
+window.addEventListener('load', getPersonagens())
